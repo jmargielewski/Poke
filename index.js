744187b4da7f5fd37a31bf8117b3a102d95e68a7
kuba <kuba.margielewski@gmail.com>
2018-11-04 12:35:53 +0100

    logging requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const morgan = require('morgan');
 const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
 require('./models/User');
@@ -9,6 +10,9 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+// Morgan - logging incoming request
+app.use(morgan('combined'));
 
 app.use(
   cookieSession({
@@ -23,4 +27,6 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
+// const server = http.createServer(app);
+// server.listen(PORT);
 app.listen(PORT);
