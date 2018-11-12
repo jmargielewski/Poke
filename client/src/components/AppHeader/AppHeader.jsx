65855e607f60b64a7dcb0122eae7e4d9f3569922
kuba <kuba.margielewski@gmail.com>
2018-11-12 22:55:51 +0100

    BEMHelper vs StyledComponent

--- client/src/components/AppHeader/AppHeader.jsx
+++ client/src/components/AppHeader/AppHeader.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import BEMHelper from 'react-bem-helper';
+import { Header } from './AppHeaderStyles';
 
 import './appHeader.scss';
 
 const bem = BEMHelper('app-header');
 
 export default ({ leftPart, rightPart }) => (
-  <header {...bem()}>
+  <Header>
     <nav {...bem('container')}>
       <div {...bem('left')}>{leftPart}</div>
       <div {...bem('right')}>{rightPart}</div>
     </nav>
-  </header>
+  </Header>
 );
