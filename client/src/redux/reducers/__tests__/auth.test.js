32b2981ece06df0e6d4fb18a34410d0697d38640
kuba <kuba.margielewski@gmail.com>
2018-11-17 16:59:41 +0100

    testing auth reducer with other actions

--- client/src/redux/reducers/__tests__/auth.test.js
+++ client/src/redux/reducers/__tests__/auth.test.js
@@ -6,10 +6,45 @@ import {
   AUTH_USER_LOGOUT,
 } from 'redux/actions/types';
 
-it('handles action of unknown type', () => {
-  const action = { type: 'UNKNOWN' };
+describe('Auth Reducer', () => {
+  it('handles action of unknown type', () => {
+    const action = { type: 'UNKNOWN' };
+    const newState = authReducer(INITIAL_STATE, action);
 
-  const newState = authReducer(INITIAL_STATE, action);
+    expect(newState).toEqual(INITIAL_STATE);
+  });
 
-  expect(newState).toEqual(INITIAL_STATE);
+  it('handles action of type AUTH_USER', () => {
+    const action = { type: AUTH_USER };
+    const newState = authReducer(INITIAL_STATE, action);
+
+    expect(newState).toEqual({ ...INITIAL_STATE, loading: true });
+  });
+
+  it('handles action of type AUTH_USER_SUCCESS', () => {
+    const action = {
+      type: AUTH_USER_SUCCESS,
+      payload: 'jbJy5XSgX13TS8',
+    };
+    const newState = authReducer(INITIAL_STATE, action);
+
+    expect(newState).toEqual({ ...INITIAL_STATE, authenticated: action.payload, loading: false });
+  });
+
+  it('handles action of type AUTH_USER_FAILURE', () => {
+    const action = {
+      type: AUTH_USER_FAILURE,
+      payload: 'failure',
+    };
+    const newState = authReducer(INITIAL_STATE, action);
+
+    expect(newState).toEqual({ ...INITIAL_STATE, errorMessage: action.payload, loading: false });
+  });
+
+  it('handles action of type AUTH_USER_LOGOUT', () => {
+    const action = { type: AUTH_USER_LOGOUT };
+    const newState = authReducer(INITIAL_STATE, action);
+
+    expect(newState).toEqual({ ...INITIAL_STATE, authenticated: '' });
+  });
 });
