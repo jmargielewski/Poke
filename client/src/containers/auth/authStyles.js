991666963481d9c5ab9cff6aeada786284f6efa1
kuba <kuba.margielewski@gmail.com>
2018-11-25 23:39:23 +0100

    add falling down animation

--- client/src/containers/auth/authStyles.js
+++ client/src/containers/auth/authStyles.js
@@ -1,10 +1,29 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fallingDown = keyframes`
+  0% {
+    margin-top: -100px;
+    opacity: .75;
+  }
+  50% {
+    margin-top: 60px;
+  }
+  75% {
+    margin-top: 48px;
+  }
+  100% {
+    margin-top: 50px;
+    opacity: 1;
+  }
+`;
+
 
 const AuthWrap = styled.div`
   width: 400px;
   background: white;
   margin: 50px auto;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .25);
+  animation: ${fallingDown} 1s ease-out 1;
 `;
 
 const AuthHeader = styled.div`
