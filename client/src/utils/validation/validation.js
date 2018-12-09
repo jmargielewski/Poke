38a9a08c08ec3b90bb3a3ba9a601b48a581d4827
jmargielewski <kuba.margielewski@gmial.com>
2018-12-09 10:54:11 +0100

    test commit

--- client/src/utils/validation/validation.js
+++ client/src/utils/validation/validation.js
@@ -2,6 +2,7 @@ export const isRequire = value => (
   value ? undefined : 'Require field'
 );
 
+
 // !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 export const validateEmail = value => (
   value.includes('@') ? undefined : 'Email is invalid'
